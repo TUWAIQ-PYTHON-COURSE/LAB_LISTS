825834b1a21f81f14677d66b0d38442c8f4e0ae7
anwaar <az.1221.anwar@gmail.com>
2022-07-03 17:32:23 +0300

    Lists

--- lists.py
+++ lists.py
@@ -2,7 +2,6 @@
 
 ## Given the following list : [5, 4, 17, 19, 30, 2, 7, 10, 45]
 
-from hashlib import new
 
 
 my_list = [5, 4, 17, 19, 30, 2, 7, 10, 45]
@@ -17,11 +16,8 @@ ma= max(my_list)
 print(ma)
 
 ### Q3: using List Comprehension , create a new list from the above list containing only even numbers.
-even_list = []
-for x in my_list:
-        if x % 2 == 0:
-            print (x)          
-        even_list.append(x)
+evensList = [x for x in my_list if x % 2 == 0]
+print(evensList)
 
 ### Q4: use list slicing to get a new list from the previous list starting from the start to the 5th element in the list
 
