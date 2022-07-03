0386023114b7da7d2cd28321156133837ecaf373
anwaar <az.1221.anwar@gmail.com>
2022-07-03 17:23:38 +0300

    Lists

---
+++ lists.py
@@ -0,0 +1,30 @@
+# LAB_LISTS
+
+## Given the following list : [5, 4, 17, 19, 30, 2, 7, 10, 45]
+
+from hashlib import new
+
+
+my_list = [5, 4, 17, 19, 30, 2, 7, 10, 45]
+
+### Q1: Write a Python program to sum all the items in the list.
+sum = sum(my_list)
+print(sum)
+
+### Q2: Write a Python program to get the largest number from the list.
+
+ma= max(my_list)
+print(ma)
+
+### Q3: using List Comprehension , create a new list from the above list containing only even numbers.
+even_list = []
+for x in my_list:
+        if x % 2 == 0:
+            print (x)          
+        even_list.append(x)
+
+### Q4: use list slicing to get a new list from the previous list starting from the start to the 5th element in the list
+
+new_list2 = my_list[1:5]
+
+print(new_list2)
