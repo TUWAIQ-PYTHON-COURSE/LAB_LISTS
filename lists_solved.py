00d8b0b27cab65f988a9003bb37c45a225ee997a
Omar A Almutairi <omaramm2009@gmail.com>
2022-07-03 18:13:11 +0300

    lists lab solved

---
+++ lists_solved.py
@@ -0,0 +1,12 @@
+
+my_list = [5, 4, 17, 19, 30, 2, 7, 10, 45]
+#code 1 
+sum_list = sum(my_list)
+print(sum_list)
+#code 2
+print(max(my_list))
+#code 3 
+my_list2 = [numbers for numbers in my_list]
+print(my_list2)
+#code 4 
+print(my_list[0:6])
